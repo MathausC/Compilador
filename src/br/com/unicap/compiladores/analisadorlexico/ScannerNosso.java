a24b7e8c4611a1fe29948615d28d46f90a2eb92c
MathausC <mathaus273@gmail.com>
2021-03-09 15:40:22 -0300

    Acrescentando funçoes auxiliares

--- src/br/com/unicap/compiladores/analisadorlexico/ScannerNosso.java
+++ src/br/com/unicap/compiladores/analisadorlexico/ScannerNosso.java
@@ -1,8 +1,44 @@
 package br.com.unicap.compiladores.analisadorlexico;
 
-public class ScannerNosso {
-    public ScannerNosso(String file){
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class ScannerNosso {
+    private char[] conteudo;
+
+    public ScannerNosso(String file){
+        try {
+            String s;
+            s = new String(Files.readAllBytes(Paths.get(file)), StandardCharsets.UTF_8);
+            conteudo = s.toCharArray();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
     }
 
+    private boolean isDigit(char c) {
+        return c >='0' && c <= '9';
+    }
+
+    private boolean isLowCase(char c) {
+        return c >= 'a' && c <= 'z';
+    }
+
+    private boolean isUpperCase(char c) {
+        return c >= 'A' && c <= 'Z';
+    }
+
+    private boolean isLetter(char c) {
+        return (isLowCase(c) || isUpperCase(c));
+    }
+
+    private boolean isIgnorable(char c) {
+        return (c == ' ' || c =='\n' || c == '\t' || c == '\r');
+    }
+
+    private boolean isOperator(char c) {
+        return (c == '>' || c == '<' || c == '=' || c == '!');
+    }
 }
