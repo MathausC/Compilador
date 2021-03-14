aa7fdb0289e44ed0422ba8e1395424bc015d4c63
Almir Junior <almir.coder@gmail.com>
2021-03-14 15:45:17 -0300

    Adicionando o tratamento de erros no main.

--- src/br/com/unicap/compiladores/main/Main.java
+++ src/br/com/unicap/compiladores/main/Main.java
@@ -3,11 +3,18 @@ package br.com.unicap.compiladores.main;
 import java.util.ArrayList;
 
 import br.com.unicap.compiladores.analisadorlexico.*;
+import br.com.unicap.compiladores.excecoes.LexicalException;
 
 public class Main {
     public static void main(String[] args) {
-        ScannerNosso s = new ScannerNosso("teste.txt");
-        ArrayList<Token> tokens = s.getTokens();
-        System.out.println(tokens);
+        try {
+            ScannerNosso s = new ScannerNosso("teste.txt");
+            ArrayList<Token> tokens = s.getTokens();
+            System.out.println(tokens);
+        }catch (LexicalException a){
+            System.out.println("ERRO: " + a.getMessage());
+        }catch (Exception b){
+            System.out.println("ERRO GENERICO");
+        }
     }
 }
